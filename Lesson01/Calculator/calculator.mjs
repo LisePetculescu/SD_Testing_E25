b30a7b113531b0718d64f74a1fedfa7316134056
LisePetculescu <lise.lili97gmail.com>
2025-08-31 17:41:25 +0200

    calculator with jest and vitest unit tests

---
+++ Lesson01/Calculator/calculator.mjs
@@ -0,0 +1,33 @@
+
+
+function sum(a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Incorrect parameter type');
+    }
+    return a + b;
+};
+
+function subtract(a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Incorrect parameter type');
+    }
+    return a - b;
+};
+
+function multiply(a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Incorrect parameter type');
+    }
+    return a * b;
+};
+
+function divide(a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Incorrect parameter type');
+    }
+    return a / b;
+}
+
+
+export { sum, subtract, multiply, divide };
+
